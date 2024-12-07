68724eb773cd0f0524c67377f0138b1ad3752d8b
Cristhianjoro <cristhianjoro06@gmail.com>
2024-12-07 13:54:46 -0500

    registro de usuarios agregado

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -2,6 +2,7 @@ import { NgClass, NgIf } from '@angular/common';
 import { Component, NgModule, NgModuleRef } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Ejemplo válido
 import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { AuthService } from '../../servicios/auth.service';
 
 
 @Component({
@@ -12,23 +13,26 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule
 })
 export class LoginComponent {
   state = LoginCompState.LOGIN;
-//login
-email: string = '';
-password: string = '';
+//loglin
+emaill: string = '';
+passwordl: string = '';
  // Variables para capturar los datos del formulario
  name: string = '';
  lastname: string = '';
  career: string = '';
- emailr: string = '';
- passwordr: string = '';
+ email: string = '';
+ password: string = '';
  passwordconf: string = '';
+ constructor(private authService: AuthService){
+
+ }
 
 
 // Método que se ejecuta al hacer clic en el botón de iniciar sesión
 btnLogin() {
   
-  console.log('Correo Electrónico:', this.email);
-  console.log('Contraseña:', this.password);
+  console.log('Correo Electrónico:', this.emaill);
+  console.log('Contraseña:', this.passwordl);
 }
 btnRegister(){
   console.log('Nombre:', this.name);
@@ -37,8 +41,38 @@ btnRegister(){
   console.log('Correo Electrónico:', this.email);
   console.log('Contraseña:', this.password);
   console.log('Confirmar Contraseña:', this.passwordconf);
+
+  
+
+
+
+if(!this.vacio(this.email) && !this.vacio(this.password) && !this.vacio(this.passwordconf) && !this.vacio(this.name)
+  && !this.vacio(this.lastname) && !this.vacio(this.career)
+  ){
+    alert("Completa todos los campos")
+  } else if(!this.igual(this.password, this.passwordconf)){
+    alert("La contraseña no coincide")
+  }else if(this.password.length <6){
+    alert("La contraseña debe tener minimo 6 caracteres")
+
+  }
+
+  else{
+    this.authService.registro(this.email,this.password,this.name,this.lastname,this.career)
+
+  }
+
+
 }
 
+vacio(text: string) {
+  return text != null && text.length > 0;
+}
+
+igual(text: string, text2: string) {
+  return text == text2;
+
+}
 
   ForgotPassClick() {
     this.state = LoginCompState.FORGOT_PASSWORD;
---
+++ src/environment/environment.ts
@@ -0,0 +1,18 @@
+
+
+export const environment = {
+    production: false,
+    firebaseConfig : {
+      apiKey: "AIzaSyCBRFyJkeWbHzWdOhxotsaJ2geLbEDnROA",
+      authDomain: "example-83e93.firebaseapp.com",
+      projectId: "example-83e93",
+      storageBucket: "example-83e93.firebasestorage.app",
+      messagingSenderId: "1085176755401",
+      appId: "1:1085176755401:web:b2ea83fc458b873d550918",
+      
+        
+      }
+    
+    
+    }
+  
