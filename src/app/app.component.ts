4f290a0abe0ed799a5fce5dfa502f7e4b3a566a8
Cristhianjoro <cristhianjoro06@gmail.com>
2024-12-07 13:10:34 -0500

    Diseño de login agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './paginas/header/header.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RedSocial';
+  login : boolean = false;
 }
