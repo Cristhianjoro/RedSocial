3b70f6dd46e5e1414c517b234b22edff2bc6311d
Cristhianjoro <cristhianjoro06@gmail.com>
2024-12-07 12:49:59 -0500

    Rutas agregadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PerfilComponent } from './paginas/perfil/perfil.component';
+import { GrupoComponent } from './paginas/grupo/grupo.component';
+import { EventoComponent } from './paginas/evento/evento.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+   
+    {path: "perfil", component: PerfilComponent},
+    {path: "grupos", component: GrupoComponent},
+    {path: "eventos", component: EventoComponent},
+];
