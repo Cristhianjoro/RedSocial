444137d0f8667848f883f865fc4a72baa216d4ac
Cristhianjoro <cristhianjoro06@gmail.com>
2024-12-07 13:02:27 -0500

    Diseño de feed y grupo agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { PerfilComponent } from './paginas/perfil/perfil.component';
 import { GrupoComponent } from './paginas/grupo/grupo.component';
 import { EventoComponent } from './paginas/evento/evento.component';
+import { FeedComponent } from './paginas/feed/feed.component';
 
 export const routes: Routes = [
 
-   
+    {path: "", component: FeedComponent},
     {path: "perfil", component: PerfilComponent},
     {path: "grupos", component: GrupoComponent},
     {path: "eventos", component: EventoComponent},
